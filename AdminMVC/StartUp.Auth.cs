de9f19164afd658f1263750a2a2d625a984213e9
Hassan Liasu <htope68@gmail.com>
2018-04-13 16:38:58 +0100

    Initial

---
+++ AdminMVC/StartUp.Auth.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminMVC
+{
+	public partial class StartUp
+	{
+        public static void ConfigureAuth(IAppBuilder app)
+        {
+            var option = new CookieAuthenticationOptions()
+            {
+				AuthenticationType = "ApplicationCookie",
+                CookieName = "Admin",
+                LoginPath = new PathString("/Auth/Login")
+            };
+            app.UseCookieAuthentication(option);
+        }
+    }
+}
---
+++ AdminMVC/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using AdminMVC;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(StartUp))]
+namespace AdminMVC
+{
+    public partial class StartUp
+    {
+            public static void Configuration(IAppBuilder app)
+            {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ConfigureAuth(app);
+            }
+    }
+}
